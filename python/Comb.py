dab576eef2c2af69658fb02bf33d078161068dbb
tugberk92 <atakan.tugberk.akmete@cern.ch>
2019-03-13 10:42:50 +0100

    exclusion

--- python/Comb.py
+++ python/Comb.py
@@ -15,13 +15,7 @@ for o,a in opts:
 
 pathW = "../data/"+date+"/"
 
-data1  = open(pathW+'qcd_Ana_rate1.dat')
-data2  = open(pathW+'meson_Ana_rate1.dat')
-data3  = open(pathW+'pbrem_Ana_rate1.dat')
-
-sum1   = open(pathW+'meson_Ana_sum.dat','r')
-sum2   = open(pathW+'pbrem_Ana_sum.dat','r')
-sum3   = open(pathW+'qcd_Ana_sum.dat','r')
+data1  = open(pathW+'comb_Ana_rate1.dat')
 Rate   = open(pathW+'Rate1.dat','w')
 
 line_1 = data1.readlines()
@@ -36,24 +30,14 @@ def find_Rate(lines,mass,eps):
     for i in lines:
         k = i.replace('\n','')
         k = k.split(' ')
-        if abs(math.log10(float(k[1])) - math.log10(eps)) <0.1 and abs(float(k[0]) - mass)<0.0001: return float(k[2])
+        if abs(math.log10(float(k[1])) + eps) <0.1 and abs(float(k[0]) - mass)<0.0001: return float(k[2])
     else: return 0
-
-def find_N(lines,eps,mass):
-    for i in lines:
-        k = i.replace('\n','')
-        k = k.split(' ')
-        if abs(math.log10(float(k[1])) - math.log10(eps)) <0.1 and abs(float(k[0]) - mass)<0.0001: return float(k[4])
-    else:
-        print "something is wrong"
-        return 0
-
 
 for i in line_1:#meson rates with pbrem shared
     #Rate_tot=0.
     i = i.replace('\n','')
     i = i.split(' ')
-    p = find_Rate( line_3, float(i[0]), float(i[1]) )
+    p = find_Rate( line_2, float(i[0]), float(i[1]) )
     if p:
         #N1 = find_N( line_s1, float(i[0]),float(i[1]) )
         #N2 = find_N( line_s2, float(i[0]), float(i[1]) )
@@ -64,10 +48,10 @@ for i in line_1:#meson rates with pbrem shared
     Rate.write('%.4g %.8g %.8g' %(float(i[0]), float(i[1]), Rate_tot))
     Rate.write('\n')
 
-for i in line_2:#pbrem just in case
+for i in line_3:#pbrem just in case
     i = i.replace('\n','')
     i = i.split(' ')
-    p = find_Rate( line_3, float(i[0]), float(i[1]) )
+    p = find_Rate( line_2, float(i[0]), float(i[1]) )
     if p:
         #N3 = find_N( line_s3, float(i[0]),float(i[1]) )
         #N2 = find_N( line_s2, float(i[0]), float(i[1]) )
--- python/Excl_1.py
+++ python/Excl_1.py
@@ -22,40 +22,42 @@ if leptophilic:
     data1 = pd.read_csv(pathW+'qcd_Ana_rate2.csv', header=None)
     data2 = pd.read_csv(pathW+'meson_Ana_rate2.csv', header=None)
     data3 = pd.read_csv(pathW+'pbrem_Ana_rate2.csv', header=None)
+    dataC = pd.read_csv(pathW+'Rate2.csv', header=None)
 else:
     data1 = pd.read_csv(pathW+'qcd_Ana_rate1.csv', header=None)
     data2 = pd.read_csv(pathW+'meson_Ana_rate1.csv', header=None)
     data3 = pd.read_csv(pathW+'pbrem_Ana_rate1.csv', header=None)
-
+    #dataC = pd.read_csv(pathW+'comb_Ana_rate1.csv', header=None)
+    dataC = pd.read_csv(pathW+'comb.csv', header=None)
 ##### Constraints
 dataC1 = pd.read_csv(pathR+'Dataset_DP_excluded_u.csv', header=None)
 dataC2 = pd.read_csv(pathR+'Dataset_DP_excluded_l.csv', header=None)
 
-
 y2 = np.array([1e-2]*len(dataC1[1]))
 #y3=np.array([1.0e-4]*len(data3[1]))
 #y4=np.array([200.00]*len(data3[1]))
+
 import matplotlib
 matplotlib.use("TkAgg")
 import matplotlib.pyplot as pl
 
-
 pl.yscale('log')
 pl.xscale('log')
 
-"""pl.fill(data1[0], data1[1], 'b', label='SHiP (QCD)')
-#pl.fill_between(data3[0], data3[1], where=data3[0] > y3  , color='green', label="SHiP (Brem)")
-pl.fill(data3[0], data3[1], 'g', label="SHiP (Brem)") 
-pl.fill(data2[0], data2[1], 'r', label="SHiP (Mesons)")"""
+pl.plot(data1[0], data1[1], 'b', label='SHiP (QCD)')
+pl.plot(data3[0], data3[1], 'g', label='SHiP (Pbrem)')
+pl.plot(data2[0], data2[1], 'r', label='SHiP (Mesons)')
+pl.plot(dataC[0], dataC[1], 'black', label='SHiP (Combined)')
 
-pl.fill_between(data1[0], data1[1], y2=0, color='red', hatch='.', label='SHiP (QCD)')
-pl.fill_between(data3[0], data3[1], y2=0, color='blue', label="SHiP (Brem)") 
-pl.fill_between(data2[0], data2[1], y2=0, color='green', label="SHiP (Mesons)")
+#pl.fill_between(data1[0], data1[1], y2=0, color='red', hatch='.', label='SHiP (QCD)')
+#pl.fill_between(data3[0], data3[1], y2=0, color='blue', label="SHiP (Brem)") 
+#pl.fill_between(data2[0], data2[1], y2=0, color='green', label="SHiP (Mesons)")
 
 #pl.fill_between(data3[0], data3[1], where=data3[0] > y3  , color='green', label="SHiP (Brem)")
 
 pl.fill_between(dataC1[0], dataC1[1], y2=y2, color="grey", label="Excluded Region")
 pl.fill_between(dataC2[0], dataC2[1], y2=0, color="grey")
+ 
 pl.xlabel("M [MeV]",fontsize=18)
 pl.ylim(1e-10, 1e-2)
 pl.xlim(13, 1e4)
