96900b6d46c88a93c85ffc60995bbfc3907847b8
Atakan Tugberk <tugberk92@gmail.com>
2020-04-01 16:25:58 +0300

    Exclusion Plots are ready.

--- python/Order.py
+++ python/Order.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os,sys,getopt
 try:
     opts, args = getopt.getopt(sys.argv[1:], "d:", ["date="]) 
 except getopt.GetoptError:
@@ -7,12 +8,14 @@ except getopt.GetoptError:
 for o,a in opts:
     if o in ('-d','--date',): date = a
 
-prods = ['pbrem','pbrem1','meson','qcd','comb','comb1']
+#prods = ['pbrem','pbrem1','meson','qcd','comb','comb1']
+prods = ['pbrem','meson','qcd','comb']
 for prod in prods:
     inp="../data/"+date+"/"+prod+"_Rate1.csv"
-    data = pd.read_csv(path,header=None)
-    data.head()
+    data = pd.read_csv(inp,header=None)
+    #data.head()
     #new=data.sort_values(by=[1,0],ascending=[False,True])
     new=data.sort_values(by=1,ascending=False)
     new.to_csv(inp,index=False) 
+
 
