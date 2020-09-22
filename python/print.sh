c746f59014b7a053131f40fd59ce1a3a653841e4
Atakan Tugberk Akmete <takmete@Atakans-Mini.home>
2020-09-22 20:03:21 +0300

    mumu kinematics are ready

--- python/print.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for mod in "$@"; do {
-  echo Eval_1.py -d 200922 -p "$mod" -a Rate1
-  echo Eval_1.py -d 200922 -p "$mod" -a ErrorRateM
-  echo Eval_1.py -d 200922 -p "$mod" -a ErrorRateP
-} done
